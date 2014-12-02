cb59fb80817107d67e024915e5c0dbd26a8e7092
Elvin Yung <elvinyung1@gmail.com>
2014-12-01 20:16:56 -0500

    basic quickstart example

--- examples/quickstart-example/server.js
+++ examples/quickstart-example/server.js
@@ -1,18 +1,18 @@
 var epsilonDelta = require('./../../'),
-    express = require('express');
+  express = require('express');
 
 var app = express();
 
 var limiter = epsilonDelta({
-    userKey: 'connection.remoteAddress', // identify users by IP
-    capacity: 100, // 200 requests
-    expire: 1000 * 60 * 60 * 1, // 1 hour
-    limitResponse: {
-        message: "Sorry! You're all out for now."
-    }
+  userKey: 'connection.remoteAddress', // identify users by IP
+  capacity: 100, // 200 requests
+  expire: 1000 * 60 * 60 * 1, // 1 hour
+  limitResponse: {
+    message: "Sorry! You're all out for now."
+  }
 });
 app.use(limiter.middleware);
 
 app.get('/', function (req, res) {
-    res.send(200, 'Hello world!');
+  res.send(200, 'Hello world!');
 });
