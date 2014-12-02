44e1e73977d3a8648409265e7a4cfff34df077e9
Elvin Yung <elvinyung1@gmail.com>
2014-12-01 21:13:21 -0500

    fixed example

--- examples/quickstart-example/server.js
+++ examples/quickstart-example/server.js
@@ -5,14 +5,16 @@ var app = express();
 
 var limiter = epsilonDelta({
   userKey: 'connection.remoteAddress', // identify users by IP
-  capacity: 100, // 200 requests
+  capacity: 10, // 200 requests
   expire: 1000 * 60 * 60 * 1, // 1 hour
   limitResponse: {
     message: "Sorry! You're all out for now."
   }
 });
-app.use(limiter.middleware);
+app.use(limiter);
 
 app.get('/', function (req, res) {
-  res.send(200, 'Hello world!');
+  res.status(200).send('Hello world!');
 });
+
+app.listen(3000);
