20d4829a50b2e66981099015e16c1723c88c8693
Elvin Yung <elvinyung1@gmail.com>
2015-03-25 22:29:52 -0400

    Add limiter tests

--- test/limiter.js
+++ test/limiter.js
@@ -35,4 +35,38 @@ describe('limiter', function () {
       });
     });
   });
+
+  describe('rate', function () {
+    it('should correctly rate limit a user', function (done) {
+      var limiter = epsilonDelta({
+        capacity: 10, // 100 requests
+      });
+      var username = 'someone';
+
+      for (var a = 0; a < 10; a++) {
+        limiter.rate(username);
+      }
+
+      limiter.rate(username, function (err, limitReached) {
+        assert.equal(limitReached, true);
+        done();
+      });
+    });
+
+    it('should correctly let non limit reached user through', function (done) {
+      var limiter = epsilonDelta({
+        capacity: 10, // 100 requests
+      });
+      var username = 'someone';
+
+      for (var a = 0; a < 5; a++) {
+        limiter.rate(username);
+      }
+
+      limiter.rate(username, function (err, limitReached) {
+        assert.equal(limitReached, false);
+        done();
+      });
+    });
+  });
 });
