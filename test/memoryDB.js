4b7de3394ba53a60b8df58d81101b44501a93aaa
Elvin Yung <elvinyung1@gmail.com>
2015-03-31 18:56:20 -0400

    Add tests for memoryDB hmset

--- test/memoryDB.js
+++ test/memoryDB.js
@@ -17,4 +17,24 @@ describe('memoryDB', function () {
       })
     });
   });
+
+  describe('hmset', function () {
+    it('should properly hmset something to memoryDB', function () {
+      db.hmset('somekey', {foo: 'somevalue'}, function (err) {
+        db.get('somekey', function (err, data) {
+          assert.deepEqual(data, {foo: 'somevalue'});
+        });
+      })
+    });
+
+    it('should properly hmset reset something to memoryDB', function () {
+      db.hmset('somekey', {foo: '1'}, function (err) {
+        db.hmset('somekey', {foo: '2'}, function (err) {
+          db.get('somekey', function (err, data) {
+            assert.deepEqual(data, {foo: '2'});
+          });
+        });
+      })
+    });
+  });
 });
